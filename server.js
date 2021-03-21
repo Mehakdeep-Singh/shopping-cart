39197f080e0235c481df751144ae60f5123e706a
Mehakdeep Chhina <GRAZITTI\mehakdeep.chhina@mehakdeep-chhina-linux.grazitti.local>
2021-03-22 00:04:39 +0530

    code cmnted

--- server.js
+++ server.js
@@ -97,11 +97,11 @@ if(process.env.NODE_ENV === 'production'){
     
 }
 
-app.use(express.static(path.join(__dirname, 'build')));
-// Handle React routing, return all requests to React app
-app.get('*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.use(express.static(path.join(__dirname, 'build')));
+// // Handle React routing, return all requests to React app
+// app.get('*', function(req, res) {
+//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 
 
 const port = process.env.PORT || 4000;

fd3416ca2b5b185eb3762e35a9603f0bf684984a
Mehakdeep Chhina <mehakdeepchhina@Mehakdeeps-MacBook-Air.local>
2021-12-27 23:45:49 -0330

    make it better

--- server.js
+++ server.js
@@ -104,5 +104,5 @@ if(process.env.NODE_ENV === 'production'){
 // });
 
 
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("running on port ",port))
